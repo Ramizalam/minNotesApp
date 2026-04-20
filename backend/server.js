a626ff5d8597d17d92aa817688226048e0b39569
Ramizalam <ramizalam63@gmail.com>
2026-04-20 18:16:47 +0530

    added port

--- backend/server.js
+++ backend/server.js
@@ -8,11 +8,11 @@ const authRoutes = require("./routes/authRoutes");
 const noteRoutes = require("./routes/noteRoutes");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT ;
 
 // Middleware
 app.use(cors({
-  origin: process.env.CLIENT_URL || "http://localhost:5173",
+  origin: process.env.CLIENT_URL,
   credentials: true,
 }));
 app.use(express.json());
